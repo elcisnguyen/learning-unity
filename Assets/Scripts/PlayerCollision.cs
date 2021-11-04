0907508b24a316eb755ee923481c603b0b73933a
Nguyễn Tấn Hào <tanhaonguyen2015@gmail.com>
2021-11-04 18:55:32 +0700

    Ep07: Add Text to game
    - Add score text to game screen.
    - Update Unity Tutorial document.

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement playerMovement;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Obstacle")
+        {
+            Debug.Log("We hit obstacle");
+            playerMovement.enabled = false; //Make player stop moving when collision happens
+        }
+    }
+}

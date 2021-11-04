154d502881e2bf88c94249a51657c232f8afc700
Nguyễn Tấn Hào <tanhaonguyen2015@gmail.com>
2021-11-04 14:44:21 +0700

    Ep03: Friction and Object movement
    - Add just friction of object.
    - Control object movement by keyboard.
    - Update Unity Tutorial document.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
   
     void Start()
     {
@@ -11,6 +13,16 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+        //Add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
